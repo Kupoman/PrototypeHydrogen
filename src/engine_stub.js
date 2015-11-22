a094d73c68776ad6ce85605f1eb4f8a13c4c16ec
Mitchell Stokes <mogurijin@gmail.com>
2015-11-21 21:43:46 -0800

    Adding some basic attack code

--- src/engine_stub.js
+++ src/engine_stub.js
@@ -2,12 +2,15 @@
 // intended to work with Panda3D (e.g., utils) should go in their
 // own JS file.
 
-;(function () {
-    function load_mech_data (uri) {
+Engine = {
+    mechs: [],
+
+    load_mech_data: function (uri) {
         $.ajax({
             url: uri,
             dataType: 'json',
             success: function (data) {
+                Engine.mechs.push(data)
                 add_mech(data)
             },
             error: function (xhr, status, error) {
@@ -15,24 +18,35 @@
                 console.log('failed to load ' + uri)
             }
         })
+    },
+
+    init: function () {
+        Engine.load_mech_data('mechs/mechone.json')
+        Engine.load_mech_data('mechs/mechtwo.json')
+        Engine.load_mech_data('mechs/mechone.json')
+        Engine.load_mech_data('mechs/mechtwo.json')
+        Engine.load_mech_data('mechs/mechone.json')
+    },
+
+    main: function () {
+        window.requestAnimationFrame(Engine.main)
+    },
+
+    do_attack: function () {
+        Engine.mechs.forEach(function (mech) {
+            if (mech.weapon.resource_current > 0) {
+                mech.weapon.resource_current -= 1;
+                console.log(mech.name + " attacks!");
+            }
+            else {
+                console.log(mech.name + " is out of resources!");
+            }
+        })
     }
+}
 
-
-    function init () {
-        load_mech_data('mechs/mechone.json')
-        load_mech_data('mechs/mechtwo.json')
-        load_mech_data('mechs/mechone.json')
-        load_mech_data('mechs/mechtwo.json')
-        load_mech_data('mechs/mechone.json')
-    }
-
-    function main () {
-        window.requestAnimationFrame(main)
-    }
-
-    $(document).ready(function () {
-        init()
-        // don't need this yet
-        //main()
-    })
-})()
+$(document).ready(function () {
+    Engine.init()
+    // don't need this yet
+    //main()
+})
