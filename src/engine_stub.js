7557b5b2b15c71fd596db1f3dae66d51d53eab94
Daniel Stokes <kupomail@gmail.com>
2015-11-22 12:08:11 -0800

    Add some enemies and do some fighting!

--- src/engine_stub.js
+++ src/engine_stub.js
@@ -4,19 +4,29 @@
 
 Engine = {
     mechs: [],
+    enemies: [],
+    is_combat_over: false,
     last_mechid: 0,
 
-    load_mech_data: function (uri) {
+    load_mech_data: function (uri, is_player) {
         $.ajax({
             url: uri,
             dataType: 'json',
             success: function (data) {
-                Engine.mechs.push(data)
                 Engine.last_mechid += 1
                 data.id = Engine.last_mechid
                 data.hp_current = data.hp_max
                 data.weapon.resource_current = data.weapon.resource_max
-                add_mech(data)
+                data.initiative = 0
+                data.is_player = is_player
+                if (is_player) {
+                    Engine.mechs.push(data)
+                    add_mech(data)
+                }
+                else {
+                    data.name += data.id
+                    Engine.enemies.push(data)
+                }
             },
             error: function (xhr, status, error) {
                 console.log(error)
@@ -26,11 +36,15 @@ Engine = {
     },
 
     init: function () {
-        Engine.load_mech_data('mechs/mechone.json')
-        Engine.load_mech_data('mechs/mechtwo.json')
-        Engine.load_mech_data('mechs/mechone.json')
-        Engine.load_mech_data('mechs/mechtwo.json')
-        Engine.load_mech_data('mechs/mechone.json')
+        for (i = 0; i < 5; i++) {
+            Engine.load_mech_data('mechs/enemy.json', false)
+        }
+
+        Engine.load_mech_data('mechs/mechone.json', true)
+        Engine.load_mech_data('mechs/mechtwo.json', true)
+        Engine.load_mech_data('mechs/mechone.json', true)
+        Engine.load_mech_data('mechs/mechtwo.json', true)
+        Engine.load_mech_data('mechs/mechone.json', true)
     },
 
     main: function () {
@@ -38,18 +52,60 @@ Engine = {
     },
 
     do_attack: function () {
-        Engine.mechs.forEach(function (mech) {
-            if (mech.weapon.resource_current > 0) {
-                mech.weapon.resource_current -= 1;
-				damage_roll = Math.floor((Math.random() * 6)+1)
-				damage_mod = (mech.stats.attack - 10) / 2
-				damage = damage_roll + damage_mod
-                console.log(mech.name + " attacks for " + damage + " points of damage!")
+        if (Engine.is_combat_over)
+            return
+
+        var combatants = []
+        Engine.mechs.concat(Engine.enemies).forEach(function (combatant) {
+            if (combatant.hp_current != 0) {
+                init_roll = Math.floor((Math.random() * 21) + 1)
+                init_mod = (combatant.stats.speed - 10) / 2
+                combatant.initiative = init_roll + init_mod
+                combatants.push(combatant)
+            }
+        })
+
+        combatants.sort(function(a, b) {return a.initiative - b.initiative})
+
+        combatants.forEach(function (combatant) {
+            if (Engine.is_combat_over || combatant.hp_current <= 0) return
+
+            if (combatant.weapon.resource_current > 0) {
+                combatant.weapon.resource_current -= 1;
+                update_mech(combatant)
+
+                targets = (combatant.is_player) ? Engine.enemies : Engine.mechs
+                targets = targets.filter(function (x){ return x.hp_current > 0})
+                target = targets[Math.floor(Math.random() * targets.length)]
+                attack_roll = Math.floor((Math.random() * 21) + 1)
+                attack_mod = (combatant.stats.accuracy - 10) / 2
+                if (attack_roll + attack_mod >= (10 + target.stats.defense - 10) / 2) {
+                    damage_roll = Math.floor((Math.random() * 7) + 1)
+                    damage_mod = (combatant.stats.attack - 10) / 2
+                    damage = damage_roll + damage_mod
+                    target.hp_current = Math.max(target.hp_current - damage, 0)
+                    update_mech(target)
+                    console.log(combatant.name + " attacks " + target.name + " for " + damage + " points of damage!")
+                    if (target.hp_current == 0) {
+                        console.log(target.name + " has fallen!")
+                        if (targets.length == 1) {
+                            if (combatant.is_player)
+                                console.log("The player wins!")
+                            else
+                                console.log("The player loses :(")
+
+                            Engine.is_combat_over = true;
+                            return
+                        }
+                    }
+                }
+                else {
+                    console.log(combatant.name + " attacks " + target.name + ", but misses!")
+                }
             }
             else {
-                console.log(mech.name + " is out of resources!")
+                console.log(combatant.name + " is out of resources!")
             }
-            update_mech(mech)
         })
     }
 }
